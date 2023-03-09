046f8d77635fb4b093541c1a382eb3fd8262d377
thiagoamim <thiagoamim11@gmail.com>
2023-03-08 21:54:12 -0300

    Iniciando o projeto

---
+++ src/main/java/resources/Calculadora.java
@@ -0,0 +1,18 @@
+package resources;
+
+import jakarta.jws.WebService;
+
+@WebService (endpointInterface = "resources.ICalculadora")
+public class Calculadora implements ICalculadora {
+    @Override
+    public double somar(double x, double y) {
+        double s = x + y;
+        return s;
+    }
+
+    @Override
+    public double calcularRaizQuadrada(double x) {
+        double r = Math.sqrt(x);
+        return r;
+    }
+}
